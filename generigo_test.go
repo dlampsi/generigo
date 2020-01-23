8e9d2cdbfe42fcef2469069214508a61ac10b0f8
dlampsi <dlampsi@gmail.com>
2020-01-23 15:12:44 +0300

    Add AverageFloat64

--- generigo_test.go
+++ generigo_test.go
@@ -142,3 +142,16 @@ func Test_IsOdd(t *testing.T) {
 	f(7, true)
 	f(0, false)
 }
+
+func Test_AverageFloat64(t *testing.T) {
+	f := func(floats []float64, want float64) {
+		t.Helper()
+		av := AverageFloat64(floats)
+		if av != want {
+			t.Fatalf("unexpected average; want: %v, get: %v", want, av)
+		}
+	}
+
+	f([]float64{1.1, 1.1}, 1.1)
+	f([]float64{2.0, 1.0}, 1.5)
+}
