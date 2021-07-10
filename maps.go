59ef47d79314127a77e673947795ddb786c7ff51
dlampsi <dlampsi@gmail.com>
2021-07-10 23:54:00 +0300

    Adding JsonMapToString and StringToJsonMap

--- maps.go
+++ maps.go
@@ -1,5 +1,11 @@
 package generigo
 
+import (
+	jsoniter "github.com/json-iterator/go"
+)
+
+var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
 // Copy map into new map.
 func CopyMap(sourceMap map[string]interface{}) map[string]interface{} {
 	newMap := make(map[string]interface{})
@@ -21,4 +27,31 @@ func CopyMapString(sourceMap map[string]string) map[string]string {
 		newMap[k] = v
 	}
 	return newMap
+}
+
+// Converts map of string into string in json format.
+func JsonMapToString(m map[string]string) (string, error) {
+	var result string
+	if len(m) > 0 {
+		resBytes, err := json.Marshal(m)
+		if err != nil {
+			return "", err
+		}
+		result = string(resBytes)
+	}
+	return result, nil
+}
+
+// Converts string (in json format) to map of strings.
+func StringToJsonMap(s string) (map[string]string, error) {
+	var result map[string]string
+	if s != "" {
+		if err := json.Unmarshal([]byte(s), &result); err != nil {
+			return nil, err
+		}
+	}
+	if result == nil {
+		result = map[string]string{}
+	}
+	return result, nil
 }
--- maps_test.go
+++ maps_test.go
@@ -26,3 +26,34 @@ func Test_CopyMapString(t *testing.T) {
 	f(map[string]string{"one": "1", "two": "three"})
 	f(map[string]string{})
 }
+
+func Test_JsonMapToString(t *testing.T) {
+	f := func(m map[string]string, noerr bool, expected string) {
+		s, err := JsonMapToString(m)
+		if noerr {
+			require.NoError(t, err)
+		} else {
+			require.Error(t, err)
+		}
+		require.Equal(t, s, expected)
+	}
+
+	f(nil, true, "")
+	f(map[string]string{"one": "two"}, true, `{"one":"two"}`)
+}
+
+func Test_StringToJsonMap(t *testing.T) {
+	f := func(s string, noerr bool, expected map[string]string) {
+		m, err := StringToJsonMap(s)
+		if noerr {
+			require.NoError(t, err)
+		} else {
+			require.Error(t, err)
+		}
+		require.Equal(t, m, expected)
+	}
+
+	f("", true, map[string]string{})
+	f("string", false, nil)
+	f(`{"one":"two"}`, true, map[string]string{"one": "two"})
+}
